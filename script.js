ec9cfbb2bce017d277fcc6c6f56acfbb33b13329
MyatKo <myatkonkaungthantsoe@gmail.com>
2023-02-05 19:59:06 +0800

    base progress bar

---
+++ script.js
@@ -0,0 +1,5 @@
+function updateProgressBar(progressBar, value) {
+    value = Math.round(value);
+    progressBar.querySelector(".progress-fill").style.width = `${value}%`;
+    progressBar.querySelector(".progress-text").textContent = `${value}%`;
+}
